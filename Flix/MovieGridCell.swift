0a5366006e0d8b8b8a10953cd19725c9cb317999
= <mariadelcarmenhernandez@student.hartnell.edu>
2019-09-23 23:14:43 -0700

    fixing repo

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Maria Hernandez on 9/23/19.
+//  Copyright © 2019 Maria Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
